c92e0079fd08a847247a5849db278e3bf0e65643
khojiakbarr <chinozbekov09@gmail.com>
2024-12-15 12:43:04 +0500

    upadate

--- util/firbaseConfig.ts
+++ util/firbaseConfig.ts
@@ -1,13 +1,16 @@
 import { initializeApp } from 'firebase/app';
 
 const firebaseConfig = {
-  apiKey: 'place your FIREBASE config here',
-  authDomain: 'place your FIREBASE config here',
-  projectId: 'place your FIREBASE config here',
-  storageBucket: 'place your FIREBASE config here',
-  messagingSenderId: 'place your FIREBASE config here',
-  appId: 'place your FIREBASE config here',
+  apiKey: "AIzaSyCN-eNFoK-b5055RHiC0BEr2p-AGEaES98",
+  authDomain: "homework-app-3cad7.firebaseapp.com",
+  databaseURL: "https://homework-app-3cad7-default-rtdb.firebaseio.com",
+  projectId: "homework-app-3cad7",
+  storageBucket: "homework-app-3cad7.appspot.com",
+  messagingSenderId: "579885565243",
+  appId: "1:579885565243:web:25c0bacb5340cf8fd3e31e",
+  measurementId: "G-G7D0L5FZJ4"
 };
+
 
 const app = initializeApp(firebaseConfig);
 
